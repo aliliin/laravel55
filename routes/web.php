ae98b028b3b51cf59bbc235dd25bc944630fb549
aliliin <fonliny@gmail.com>
2017-09-23 18:41:48 +0800

    Finish user signup

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ Route::get('/about','StaticPagesController@about')->name('about');
 
 //注册
 Route::get('/signup','UsersController@create')->name('signup');
+
+//用户相关
+Route::resource('users','UsersController');
